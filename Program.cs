7b0ae785f4b9915ffff6dcce0d3df445d5d28e6c
Maxim Romanov <114128910+Roumps@users.noreply.github.com>
2022-12-26 18:33:31 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Laba5_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 3;
+            int[] array;
+            array = new int[4];
+            array[0] = 1;
+            array[1] = 45;
+            array[2] = 2345;
+            array[3] = 43276;
+            Console.WriteLine(array.Length);
+        }
+    }
+}
+          
